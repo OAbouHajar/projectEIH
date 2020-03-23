7b288e9024b0ed45a030cd52fa314ba20e068df2
oabouhajar <smsm.sy@hotmail.com>
2020-03-23 20:20:43 +0000

    the new API

--- body-detect/post-data-to-api.py
+++ body-detect/post-data-to-api.py
@@ -5,12 +5,11 @@ import time
 
 ## connection to the firebase
 firebase = firebase.FirebaseApplication('https://projecteih.firebaseio.com', None)
-
 ## get the previouse number on the API
 url = 'https://projecteih.firebaseio.com/carlowIT.json'
 r = requests.get(url)
 x= r.json()
-preNumber = x['numberOfPeopleOut']
+preNumber = x['numberOfPeopleIN']
 
 
 ## read the argument sent from line 55 the detect.py file with the number of new people detected
@@ -20,16 +19,18 @@ help="Current number of people to send to the API")
 args = vars(ap.parse_args())
 currentNumber = args["numberOUT"]
 
+
 ## add the number coming from the API to the number coming from the RassPi
-newNumber = preNumber + currentNumber
+newNumber = preNumber- currentNumber
 
 ## The Project API
 projectData = {
-    'buildingID': 'CarlowIT',
+    'buildingID': 'Institute of Technology Carlow',
     'deviceId': 'rassPi4-MainOUT',
     'name': 'Carlow IT MAIN OUT',
-    'numberOfPeopleIn': 0,
-    'numberOfPeopleOut': newNumber,
+    'address':'Institute of Technology Carlow, Kilkenny Rd, Moanacurragh, Carlow',
+    'ericode':'R93 V960',
+    'numberOfPeopleIN': newNumber,
     'status': False,
     'timeUpdated': time.time()}
 
