37be5200a457a473d09cdb391880b60b789f0041
oabouhajar <smsm.sy@hotmail.com>
2020-03-30 17:28:49 +0100

    the IR sensor wrok with seqence now

--- body-detect/eihOUTIR.py
+++ body-detect/eihOUTIR.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
 from threading import Thread
 
-
-BEAM_PIN2 = 17
-BEAM_PIN1 = 18
+## sensors connection
+BEAM_PIN1 = 18 ##sensor Number !
+BEAM_PIN2 = 17 ##sensor Number 2
 
 def ir1First(channel):
     print('one First')
@@ -15,13 +15,15 @@ def ir1First(channel):
         print('CAMERA', channel)
         pass
 
-
+## set the pins mode
 GPIO.setmode(GPIO.BCM)
+## set up the sensors to the rass pi
 GPIO.setup(BEAM_PIN2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(BEAM_PIN1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+## If any beam break happened for the sensor 1 callback the funstion to check sensor 2
 GPIO.add_event_detect(BEAM_PIN1, GPIO.RISING, callback=ir1First, bouncetime=200)
 
 
-
+#exit the program when user press enter.
 message = input("Press enter to quit\n\n")
 GPIO.cleanup()
