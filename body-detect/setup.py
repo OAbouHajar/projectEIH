fa9a5b6b6b65bcd9bf958439c0431a4ccadef198
oabouhajar <smsm.sy@hotmail.com>
2020-04-06 20:14:07 +0100

    fix the backend

--- body-detect/setup.py
+++ body-detect/setup.py
@@ -7,37 +7,14 @@ import sys
 print("################################################################################################")
 print("###################################     SETUP START       ######################################")
 print("################################################################################################")
-## parse the DB URL and set it up as enviroument variable
-ap = argparse.ArgumentParser()
-ap.add_argument("-db", "--dburl", help="The link to the firebase database",required=True)
-ap.add_argument("-r", "--regid", help="the building ID got from the registration", required=True)
-ap.add_argument("-dv", "--deviceid", help="the device Id linked to the builiding",required=True)
-args = vars(ap.parse_args())
-db_url = args["dburl"]
-reg_id = args["regid"]
-reg_id_fixed = '-'+reg_id
-device_id = args["deviceid"]
-os.system('export FIREBASE_DB_URL="{}"'.format(db_url))
-os.system('export REG_BUILIDING_ID="{}"'.format(reg_id_fixed))
-os.system('export DEVICE_ID="{}"'.format(device_id))
-#export FIREBASE_DB_URL="https://projecteih.firebaseio.com/locations.json"
-#export REG_BUILIDING_ID="-M4Eq45nYdyBOA8GgL5r"
-#export DEVICE_ID="carlow145"
-#python3 setup.py --dburl https://projecteih.firebaseio.com/locations.json --regid M4AdyrseXVUYD8UjLEB --deviceid carlow145
-
-
-print("FIREBASE_DB_URL", os.environ['FIREBASE_DB_URL'])
-print("REG_BUILIDING_ID",os.environ['REG_BUILIDING_ID'])
-print("DEVICE_ID", os.environ['DEVICE_ID'])
-
-##installing python 
+##installing python
 os.system('sudo apt-get update')
 os.system('sudo apt-get install python3.6')
 ## Installing pip
 os.system('curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"')
-os.system('python get-pip.py')     
-os.system('python3 get-pip.py')      # For specific python version 
-## Install all python dependency 
+os.system('python get-pip.py')
+os.system('python3 get-pip.py')      # For specific python version
+## Install all python dependency
 os.system('sudo pip install requests')
 os.system('sudo pip install python-firebase')
 os.system('pip3 install firebase')
